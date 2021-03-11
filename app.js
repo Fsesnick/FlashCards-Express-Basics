3accc63d382db81215d9d5abe1ce993f64afdf89
Fsesnick <61622322+Fsesnick@users.noreply.github.com>
2021-03-11 14:40:08 +0100

    The Request Object
    bodyParser

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false}));
 
 app.set('view engine', 'pug');
 
@@ -12,6 +15,14 @@ app.get('/cards', (req, res) => {
     res.render('card', { prompt: "Who is buried in Grant's tomb?" });
 });
 
+app.get('/hello', (req, res) => {
+  res.render('hello');
+});
+
+app.post('/hello', (req, res) => {
+  console.dir(req.body);
+  res.render('hello');
+});
 
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000!')
